d94e83a7615ae87dd1be6b013f61252e3f02c953
Philipp Haller <hallerp@gmail.com>
2014-08-27 11:59:33 +0200

    Remove obsolete exception class

--- akka-actor-js/src/main/scala/akka/actor/Exceptions.scala
+++
@@ -1,9 +0,0 @@
-package akka.actor
-
-import scala.annotation.tailrec
-
-/** Base class for actors exceptions. */
-class ActorsException(message: String,
-    cause: Throwable) extends Exception(message, cause) {
-  def this(message: String) = this(message, null)
-}
