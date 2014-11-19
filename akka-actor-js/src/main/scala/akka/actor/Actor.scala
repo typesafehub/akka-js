b5686dbe4f440409e68b3c80b6f8ed6d3e4d5aad
Philipp Haller <hallerp@gmail.com>
2014-11-19 15:40:33 +0100

    Use ThreadLocal for context stack

--- akka-actor-js/src/main/scala/akka/actor/Actor.scala
+++ akka-actor-js/src/main/scala/akka/actor/Actor.scala
@@ -374,13 +374,13 @@ trait Actor {
   type Receive = Actor.Receive
 
   private[this] var _context: ActorContext = {
-    val contextStack = ActorCell.contextStack
+    val contextStack = ActorCell.contextStack.get
     if ((contextStack.isEmpty) || (contextStack.head eq null))
       throw ActorInitializationException(
         s"You cannot create an instance of [${getClass.getName}] explicitly using the constructor (new). " +
           "You have to use one of the 'actorOf' factory methods to create a new actor. See the documentation.")
     val c = contextStack.head
-    ActorCell.contextStack = null :: contextStack
+    ActorCell.contextStack.set(null :: contextStack)
     c
   }
 
